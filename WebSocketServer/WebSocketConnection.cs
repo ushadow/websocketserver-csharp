938c889803e4cee98e66d942c8d2c79d7f3ba21b
Ying Yin <clevershadow@gmail.com>
2013-08-29 18:52:31 -0400

    Working connection and disconnection.

---
+++ WebSocketServer/WebSocketConnection.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Linq;
+
+using Common.Logging;
+
+namespace WebSocketServer {
+  public class DataReceivedEventArgs {
+    public int Size { get; private set; }
+    public string Data { get; private set; }
+    public DataReceivedEventArgs(int size, string data) {
+      Size = size;
+      Data = data;
+    }
+  }
+
+  public delegate void DataReceivedEventHandler(WebSocketConnection sender, DataReceivedEventArgs e);
+  public delegate void WebSocketDisconnectedEventHandler(WebSocketConnection sender, EventArgs e);
+
+  public class WebSocketConnection : IDisposable {
+
+    #region Private members
+    private static readonly ILog log = LogManager.GetCurrentClassLogger();
+    private static readonly Int32 NumMaskBytes = 4;
+
+    private byte[] dataBuffer;  // buffer to hold the data we are reading
+    #endregion
+
+    /// <summary>
+    /// An event that is triggered whenever the connection has read some data from the client
+    /// </summary>
+    public event DataReceivedEventHandler DataReceived;
+
+    public event WebSocketDisconnectedEventHandler Disconnected;
+
+    /// <summary>
+    /// Guid for the connection - thouhgt it might be usable in some way
+    /// </summary>
+    public System.Guid GUID { get; private set; }
+
+    /// <summary>
+    /// Gets the socket used for the connection
+    /// </summary>
+    public Socket ConnectionSocket { get; private set; }
+
+    #region Constructors
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="socket">The socket on which to esablish the connection</param>
+    public WebSocketConnection(Socket socket) : this(socket, 255) {}
+
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="socket">The socket on which to esablish the connection</param>
+    /// <param name="bufferSize">The size of the buffer used to receive data</param>
+    public WebSocketConnection(Socket socket, int bufferSize) {
+      ConnectionSocket = socket;
+      dataBuffer = new byte[bufferSize];
+      GUID = System.Guid.NewGuid();
+      Listen();
+    }
+    #endregion
+
+    /// <summary>
+    /// Invoke the DataReceived event, called whenever the client has finished sending data.
+    /// </summary>
+    protected virtual void OnDataReceived(DataReceivedEventArgs e) {
+      if (DataReceived != null)
+        DataReceived(this, e);
+    }
+
+    /// <summary>
+    /// Listens for incomming data
+    /// </summary>
+    private void Listen() {
+      ConnectionSocket.BeginReceive(dataBuffer, 0, dataBuffer.Length, 0, Read, null);
+    }
+
+    /// <summary>
+    /// Send a string to the client
+    /// </summary>
+    /// <param name="str">the string to send to the client</param>
+    public void Send(string str) {
+      if (ConnectionSocket.Connected) {
+        try {
+          // send the string
+          ConnectionSocket.Send(Encoding.UTF8.GetBytes(str));
+          /*
+          writer.Write(str);
+          writer.Flush();*/
+        }
+        catch {
+          if (Disconnected != null)
+            Disconnected(this, EventArgs.Empty);
+        }
+      }
+    }
+
+    private Byte[] Decode(Byte[] dataBuffer, Int32 bytesReceived) {
+      var secondByte = dataBuffer[1];
+      Int32 length = secondByte & (Byte)127;
+      var indexFirstMask = 2;
+      if (length == 126) {
+        indexFirstMask = 4;
+      }
+      else if (length == 127) {
+        indexFirstMask = 10;
+      }
+
+      var mask = new ArraySegment<byte>(dataBuffer, indexFirstMask, NumMaskBytes);
+      var indexFirstDataByte = indexFirstMask + NumMaskBytes;
+      var decoded = new Byte[bytesReceived - indexFirstDataByte];
+      for (Int32 i = indexFirstDataByte, j = 0; i < bytesReceived; i++, j++) {
+        decoded[i] = (Byte)(dataBuffer[i] ^ mask.ElementAt<Byte>(j % NumMaskBytes));
+      }
+      return decoded;
+    }
+
+    /// <summary>
+    /// reads the incomming data and triggers the DataReceived event when done
+    /// </summary>
+    private void Read(IAsyncResult ar) {
+      try {
+        int sizeOfReceivedData = ConnectionSocket.EndReceive(ar);
+        if (sizeOfReceivedData > 1) {
+          var decoded = Decode(dataBuffer, sizeOfReceivedData);
+          var dataStr = Encoding.UTF8.GetString(decoded);
+          OnDataReceived(new DataReceivedEventArgs(decoded.Length, dataStr));
+          if (dataStr.Length == 0 || Disconnected != null) {
+            Dispose();
+            Disconnected(this, EventArgs.Empty);
+          }
+        }
+      }
+      catch (Exception e) {
+        log.Error(e);
+        Dispose();
+      }
+    }
+
+    #region cleanup
+    /// <summary>
+    /// Closes the socket
+    /// </summary>
+    public void Close() {
+      ConnectionSocket.Close();
+    }
+
+    /// <summary>
+    /// Closes the socket
+    /// </summary>
+    public void Dispose() {
+      Close();
+    }
+    #endregion
+  }
+}
---
+++ WebSocketServer/WebSocketServer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+
+using Common.Logging;
+
+namespace WebSocketServer {
+
+  public class ClientConnectedEventArgs {
+    public WebSocketConnection Client { get; private set; }
+    public ClientConnectedEventArgs(WebSocketConnection client) {
+      Client = client;
+    }
+  }
+
+  public delegate void ClientConnectedEventHandler(WebSocketServer sender, 
+      ClientConnectedEventArgs e);
+
+  public class WebSocketServer {
+    private static readonly ILog log = LogManager.GetCurrentClassLogger();
+    private static readonly String MagicString = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    private static readonly String SocketKeyPattern = "Sec-WebSocket-Key: (.*)";
+
+    public event ClientConnectedEventHandler ClientConnected;
+
+    private TcpListener tcpListener;
+
+    /// <summary>
+    /// Creates a WebSocket server instance.
+    /// </summary>
+    /// <param name="ipAddress">Server IP address.</param>
+    /// <param name="port"></param>
+    public WebSocketServer(String ipAddress, Int32 port) {
+      tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
+    }
+
+    public void Start() {
+      try {
+        tcpListener.Start();
+        Console.WriteLine("WebSocketServer started on {0}", tcpListener.LocalEndpoint);
+        ListenForClients();
+      }
+      catch (SocketException se) {
+        log.Error(se.StackTrace);
+      }
+    }
+
+    public void Stop() { tcpListener.Stop(); }
+
+    private void ListenForClients() {
+      try {
+        tcpListener.BeginAcceptTcpClient(new AsyncCallback(OnClientConnect), null);
+      }
+      catch (ObjectDisposedException ode) {
+        log.Info(ode.ToString());
+      }
+    }
+
+    private void OnClientConnect(IAsyncResult asyn) {
+      var client = tcpListener.EndAcceptTcpClient(asyn);
+      Console.WriteLine("New connection from {0}", client.Client.LocalEndPoint);
+      ShakeHands(client);
+
+      var clientConnection = new WebSocketConnection(client.Client);
+      clientConnection.Disconnected += new WebSocketDisconnectedEventHandler(OnClientDisconnect);
+      if (ClientConnected != null)
+        ClientConnected(this, new ClientConnectedEventArgs(clientConnection));
+      ListenForClients();
+    }
+
+    private void OnClientDisconnect(WebSocketConnection sender, EventArgs e) {
+      Console.WriteLine("Client disconnected.");
+    }
+
+    private void ShakeHands(TcpClient client) {
+      var stream = client.GetStream();
+      while (!stream.DataAvailable) ;
+
+      var bytes = new Byte[client.Available];
+      stream.Read(bytes, 0, bytes.Length);
+      var request = Encoding.UTF8.GetString(bytes);
+      if (new Regex("^GET").IsMatch(request)) {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("HTTP/1.1 101 Switching Protocols" + Environment.NewLine);
+        sb.Append("Connection: Upgrade" + Environment.NewLine);
+        sb.Append("Upgrade: WebSocket" + Environment.NewLine);
+        sb.Append("Sec-WebSocket-Accept: " + replyHash(request) + Environment.NewLine);
+        sb.Append(Environment.NewLine);
+        var response = Encoding.UTF8.GetBytes(sb.ToString());
+        stream.Write(response, 0, response.Length);
+      }
+    }
+
+    private String replyHash(String request) {
+      var replyStr = new Regex(SocketKeyPattern).Match(request).Groups[1].Value.Trim() + MagicString;
+      var hash = SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(replyStr));
+      return Convert.ToBase64String(hash);
+    }
+  }
+
+}
